65ad5bff10ee77e8c6e810112dc1c1300efc1afe
zqartwork <zqartworkofficial@gmail.com>
2021-03-07 17:38:02 +0800

    rwd settings.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,3 +3,22 @@
 Function Library
 -----------------
 */
+$(document).ready(function(){
+    if ($(window).height() < 800) {
+        $('.kv').css({
+            'height': '100%',
+            'margin-top': $('nav').innerHeight() - 100 + 'px'
+        })
+    }
+    $('.kv, .form').css({
+        'height': $(window).height() - $('nav').innerHeight() + 'px',
+        'margin-top': $('nav').innerHeight() + 'px'
+    })
+    
+})
+$(window).resize(function(){
+    $('.kv, .form').css({
+        'height': $(window).height() - $('nav').innerHeight() + 'px',
+        'margin-top': $('nav').innerHeight() + 'px'
+    })
+})
