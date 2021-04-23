326ff2896db0b93be0fbe37b44828e605721751c
zqartwork <zqartworkofficial@gmail.com>
2021-04-23 17:27:26 +0800

    Logo 遮蔽問題改善

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -51,3 +51,30 @@ let modalFunction = function(){
     })
 
 }
+/*
+------
+Sticky Nav
+------
+*/
+$(document).ready(function() {
+    // grab the initial top offset of the navigation 
+       var stickyNavTop = $('nav').offset().top;
+       
+       // our function that decides weather the navigation bar should have "fixed" css position or not.
+       var stickyNav = function(){
+        var scrollTop = $(window).scrollTop(); // our current vertical position from the top
+             
+        // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+        // otherwise change it back to relative
+        if (scrollTop > 5) { 
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky'); 
+        }
+    };
+    stickyNav();
+    // and run it again every time you scroll
+    $(window).scroll(function() {
+        stickyNav();
+    });
+});
